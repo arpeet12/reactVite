ce3e827fb2f6aa74304ef8d4e698e974b4981cfe
arpit nemkul <arpeet.nemkul123@gmail.com>
2026-02-27 09:18:36 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id='parent'>
+      <h1 id='child'>app</h1>
+      <h2>sheh</h2>
+  
+      <div id="chacha">chacha</div>
+    </div>
+  )
+}
+
+export default App
