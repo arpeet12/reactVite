71aa3f88ad170c08c5d5b8f6b3c8906c48f15843
arpit nemkul <arpeet.nemkul123@gmail.com>
2026-02-27 12:21:54 +0545

    components learned

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Card = () => {
   return (
     <div className='card'>
-      <h1>hi i am card</h1>
+      <h1>learing of components </h1>
     </div>
   )
 }
 
 export default Card
- b
+ 
