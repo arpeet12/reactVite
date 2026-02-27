d0a13850503e4c1a89d51a065e08983d9dbd4a6b
arpit nemkul <arpeet.nemkul123@gmail.com>
2026-02-27 09:26:03 +0545

    started using new program

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div id='parent'>
-      <h1 id='child'>app</h1>
-      <h2>sheh</h2>
-  
-      <div id="chacha">chacha</div>
-    </div>
+    <>
+    app
+    </>
   )
 }
 
